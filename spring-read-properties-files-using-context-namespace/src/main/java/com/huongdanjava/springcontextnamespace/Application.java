ca3ee0e6141954c55f4d841dd924c327e7c48122
huongdanjavacom <huongdanjava.com@gmail.com>
2017-06-24 00:33:42 +0700

    Share project spring-read-properties-files-using-context-namespace

---
+++ spring-read-properties-files-using-context-namespace/src/main/java/com/huongdanjava/springcontextnamespace/Application.java
@@ -0,0 +1,11 @@
+package com.huongdanjava.springcontextnamespace;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    }
+}
