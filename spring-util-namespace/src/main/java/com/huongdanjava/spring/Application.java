28c26d35a6b119b870bbaaf7ea15bd0b0f4bffb5
huongdanjavacom <huongdanjava.com@gmail.com>
2018-02-24 06:52:35 +0700

    Share project spring-util-namespace

--- spring-util-namespace/src/main/java/com/huongdanjava/spring/Application.java
+++ spring-util-namespace/src/main/java/com/huongdanjava/spring/Application.java
@@ -1,4 +1,6 @@
-package com.huongdanjava.springutilnamespace;
+package com.huongdanjava.spring;
+
+import java.util.Properties;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,5 +9,9 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Properties p = (Properties) context.getBean("properties");
+
+        System.out.println(p.get("app.author"));
     }
 }
