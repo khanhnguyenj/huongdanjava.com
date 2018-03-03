277da9f965155958eb9a856aa68f148609f5dd62
huongdanjavacom <huongdanjava.com@gmail.com>
2018-03-04 06:49:04 +0700

    Share spring-boot-internalresourceviewresolver

---
+++ spring-boot-internalresourceviewresolver/src/main/java/com/huongdanjava/springboot/SpringBootInternalresourceviewresolverApplication.java
@@ -0,0 +1,12 @@
+package com.huongdanjava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInternalresourceviewresolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInternalresourceviewresolverApplication.class, args);
+	}
+}
