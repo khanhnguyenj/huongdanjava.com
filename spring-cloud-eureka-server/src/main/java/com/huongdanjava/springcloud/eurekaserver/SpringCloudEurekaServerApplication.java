596fd54b75c451e17a3495706def86aec3fca6ec
huongdanjavacom <huongdanjava.com@gmail.com>
2018-04-22 05:40:55 +0700

    Share project spring-cloud-eureka-server

---
+++ spring-cloud-eureka-server/src/main/java/com/huongdanjava/springcloud/eurekaserver/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.huongdanjava.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+	}
+}
