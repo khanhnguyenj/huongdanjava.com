8ab4cdcb73ce368a9703dd17ceea6b01d6279b2f
Khanh Nguyen Huu <khanhnh2@fsoft.com.vn>
2018-05-23 14:14:53 +0700

    Update

---
+++ jpa-mssql-server/src/main/java/com/huongdanjava/jpa/Clazz.java
@@ -0,0 +1,25 @@
+package com.huongdanjava.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class Clazz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
