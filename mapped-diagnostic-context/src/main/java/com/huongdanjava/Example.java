09a4c29684d3e14d0e8be40f32b684fe8871b66d
huongdanjavacom <huongdanjava.com@gmail.com>
2018-06-01 06:07:14 +0700

    Share project mapped-diagnostic-context

---
+++ mapped-diagnostic-context/src/main/java/com/huongdanjava/Example.java
@@ -0,0 +1,27 @@
+package com.huongdanjava;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+public class Example {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(Example.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("An example with Mapped Diagnostic Context");
+
+		String user = "Khanh";
+		MDC.put("user", user);
+		LOGGER.info(": Messsage 1");
+		LOGGER.info(": Messsage 2");
+		LOGGER.info(": Messsage 3");
+
+		user = "Quan";
+		MDC.put("user", user);
+		LOGGER.info(": Messsage 1");
+		LOGGER.info(": Messsage 2");
+		LOGGER.info(": Messsage 3");
+	}
+
+}
