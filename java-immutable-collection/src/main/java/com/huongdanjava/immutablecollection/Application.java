432989e2652915b550bf739f6799792e8f773cb3
huongdanjavacom <huongdanjava.com@gmail.com>
2018-06-01 06:07:43 +0700

    Share project java-immutable-collection

---
+++ java-immutable-collection/src/main/java/com/huongdanjava/immutablecollection/Application.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.immutablecollection;
+
+import java.util.Map;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Map<String, String> students = Map.of("A", "Khanh", "B", "Quan");
+
+        System.out.println(students);
+
+    }
+}
