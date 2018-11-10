f9387b3c3f74f359a7370fb6d116bda9ade2913a
huongdanjavacom <huongdanjava.com@gmail.com>
2018-11-10 14:29:11 +0700

    Share project proejct-reactor-scheduler

---
+++ project-reactor-scheduler/src/main/java/com/huongdanjava/projectreactor/Application.java
@@ -0,0 +1,16 @@
+package com.huongdanjava.projectreactor;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Application {
+
+	public static void main(String[] args) throws InterruptedException {
+		Flux.just(5, 10, 23, 25).log()
+			.flatMap(n -> Flux.just(n * 2)).publishOn(Schedulers.elastic())
+			.subscribe(value -> System.out.println(Thread.currentThread().getName()+ " : " + value));
+
+		Thread.sleep(10000);
+	}
+
+}
