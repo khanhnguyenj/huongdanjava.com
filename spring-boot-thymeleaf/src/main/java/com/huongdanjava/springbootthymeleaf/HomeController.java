9485c16b4293072f95d585e4977aac7797e2903f
huongdanjavacom <huongdanjava.com@gmail.com>
2018-11-17 15:36:08 +0700

    Share project spring-boot-thymeleaf

---
+++ spring-boot-thymeleaf/src/main/java/com/huongdanjava/springbootthymeleaf/HomeController.java
@@ -0,0 +1,26 @@
+package com.huongdanjava.springbootthymeleaf;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate );
+
+		return "home";
+	}
+}
