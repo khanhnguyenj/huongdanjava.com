582c1bdecc2a5dc1408a80002b1cb0bd39900c13
huongdanjavacom <huongdanjava.com@gmail.com>
2018-12-22 01:02:59 +0700

    Update

---
+++ spring-boot-actuator/src/main/java/com/huongdanjava/springbootactuator/AppHealthIndicator.java
@@ -0,0 +1,14 @@
+package com.huongdanjava.springbootactuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        return Health.down().build();
+    }
+}
