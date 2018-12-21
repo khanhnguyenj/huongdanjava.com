40af9f4a54235edf191089b207bd082cda1a7f31
huongdanjavacom <huongdanjava.com@gmail.com>
2018-12-22 01:04:16 +0700

    Share spring-configuration project

---
+++ spring-configuration/src/main/java/com/huongdanjava/springconfiguration/Application.java
@@ -0,0 +1,14 @@
+package com.huongdanjava.springconfiguration;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+
+		HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+		helloWorld.print();
+	}
+}
---
+++ spring-configuration/src/main/java/com/huongdanjava/springconfiguration/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.huongdanjava.springconfiguration;
+
+public class HelloWorld {
+
+    public void print()  {
+        System.out.print("Hello World!");
+    }
+}
