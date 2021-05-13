bdc4580649dbf6fc82904fdfa46e2aa8633af1ac
Huong Dan Java <huongdanjava.com@gmail.com>
2021-05-13 19:29:24 +0700

    Share projects

---
+++ liquibase-example/src/main/java/com/huongdanjava/liquibase/Application.java
@@ -0,0 +1,26 @@
+package com.huongdanjava.liquibase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+public class Application {
+
+    public static void main(String[] args) throws SQLException, LiquibaseException {
+        Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/liquibase", "postgres", "123456");
+
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(c));
+
+        Liquibase liquibase = new Liquibase("classpath:db-changelog.xml", new ClassLoaderResourceAccessor(), database);
+
+        liquibase.update("main");
+    }
+
+}
--- spring-boot-springfox/src/main/java/com/huongdanjava/springbootspringfox/SpringBootSpringfoxApplication.java
+++ spring-boot-springfox/src/main/java/com/huongdanjava/springbootspringfox/SpringBootSpringfoxApplication.java
@@ -11,31 +11,29 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-public class SpringBootSwaggerApplication {
+public class SpringBootSpringfoxApplication {
 
-	@Bean
-	public Docket studentAPI() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.huongdanjava.springbootspringfox"))
-			.paths(PathSelectors.regex("/student.*"))
-			.build()
-			.apiInfo(metaData());
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpringfoxApplication.class, args);
+    }
 
-	private ApiInfo metaData() {
+    @Bean
+    public Docket studentAPI() {
+        return new Docket(DocumentationType.OAS_30)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.huongdanjava.springbootspringfox"))
+            .paths(PathSelectors.regex("/student.*"))
+            .build()
+            .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
         return new ApiInfoBuilder()
-        		.title("Spring Boot Swagger")
-            .description("An example about Spring Boot and Springfox for Swagger.")
+            .title("Spring Boot Swagger")
+            .description("An example about Spring Boot and Springfox for Swagger")
             .contact(new Contact("Huong Dan Java", "huongdanjava.com", "huongdanjava.com@gmail.com"))
             .build();
     }
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSwaggerApplication.class, args);
-	}
 }
