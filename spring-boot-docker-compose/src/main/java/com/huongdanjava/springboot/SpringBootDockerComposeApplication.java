03d2eded7cc36aca75d3a8977ceb05fda44854db
Khanh Nguyen <nhkhanh2406@gmail.com>
2021-09-17 08:58:23 +0700

    Update

--- spring-boot-docker-compose/src/main/java/com/huongdanjava/springboot/SpringBootDockerComposeApplication.java
+++ spring-boot-docker-compose/src/main/java/com/huongdanjava/springboot/SpringBootDockerComposeApplication.java
@@ -1,32 +1,38 @@
 package com.huongdanjava.springboot;
 
 import javax.sql.DataSource;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 @RestController
 public class SpringBootDockerComposeApplication {
 
-    @Autowired
-    private DataSource dataSource;
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(SpringBootDockerComposeApplication.class);
 
-    @RequestMapping("/hello")
-    public String helloDockerCompose() {
-        Integer idleConnection = new HikariDataSourcePoolMetadata((HikariDataSource) dataSource).getIdle();
+  @Autowired
+  private DataSource dataSource;
 
-        return "Hello Docker Compose! Idle connection to database is " + idleConnection;
-    }
+  @RequestMapping("/hello")
+  public String helloDockerCompose() {
+    LOGGER.info("Received request ...");
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDockerComposeApplication.class, args);
-    }
+    Integer idleConnection =
+        new HikariDataSourcePoolMetadata((HikariDataSource) dataSource).getIdle();
+
+    return "Hello Docker Compose! Idle connection to database is " + idleConnection;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDockerComposeApplication.class, args);
+  }
 
 }
