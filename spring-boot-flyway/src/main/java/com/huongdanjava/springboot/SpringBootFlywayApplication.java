b032a91a0f3ef0d93dd21de261943732ed42282b
Khanh Nguyen <nhkhanh2406@gmail.com>
2021-10-20 17:45:07 +0700

    Share project

---
+++ spring-boot-flyway/src/main/java/com/huongdanjava/springboot/SpringBootFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlywayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-flyway/src/test/java/com/huongdanjava/springboot/SpringBootFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
