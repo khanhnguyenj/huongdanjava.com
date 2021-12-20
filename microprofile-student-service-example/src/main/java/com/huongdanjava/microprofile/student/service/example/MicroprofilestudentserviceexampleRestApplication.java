5d9caa3dd4df4af53c042ee3e71e45d6953b09f1
Khanh Nguyen <nhkhanh2406@gmail.com>
2021-12-21 05:22:17 +0700

    Share projects

---
+++ microprofile-student-service-example/src/main/java/com/huongdanjava/microprofile/student/service/example/MicroprofilestudentserviceexampleRestApplication.java
@@ -0,0 +1,11 @@
+package com.huongdanjava.microprofile.student.service.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicroprofilestudentserviceexampleRestApplication extends Application {
+}
