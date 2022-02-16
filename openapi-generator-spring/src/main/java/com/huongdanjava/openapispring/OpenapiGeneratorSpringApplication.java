5c65046b37bbd7d983f50b9d0a60f33b89739011
Khanh Nguyen <nhkhanh2406@gmail.com>
2022-02-16 19:58:04 +0700

    Share project

---
+++ openapi-generator-spring/src/main/java/com/huongdanjava/openapispring/OpenapiGeneratorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.openapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiGeneratorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiGeneratorSpringApplication.class, args);
+	}
+
+}
---
+++ openapi-generator-spring/src/test/java/com/huongdanjava/openapispring/OpenapiGeneratorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.openapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
