77193d706a9fd2c6c410557afea8f3dc6f5a3ebb
Khanh Nguyen <nhkhanh2406@gmail.com>
2022-05-08 11:14:34 +0700

    Share project

--- spring-boot-websocket/src/main/java/com/huongdanjava/springboot/websocket/WebSocketConfiguration.java
+++ spring-boot-websocket/src/main/java/com/huongdanjava/springboot/websocket/WebSocketConfiguration.java
@@ -10,14 +10,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hello").withSockJS();
-    }
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/hello").withSockJS();
+  }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    // registry.enableSimpleBroker("/topic");
+
+    // @formatter:off
+    registry.enableStompBrokerRelay("/topic")
+        .setRelayHost("172.17.0.1")
+        .setRelayPort(61613)
+        .setSystemLogin("guest")
+        .setSystemPasscode("guest");
+    // @formatter:on
+
+    registry.setApplicationDestinationPrefixes("/app");
+  }
 }
