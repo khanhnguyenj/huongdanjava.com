783c88b0d2434ef8f4f17830a310bf0cbab7a5a6
Khanh Nguyen <nhkhanh2406@gmail.com>
2022-05-10 06:55:41 +0700

    Update project

--- spring-enabletransactionmanagement/src/main/java/com/huongdanjava/springenabletransactionmanagement/SpringEnabletransactionmanagementApplication.java
+++ spring-enabletransactionmanagement/src/main/java/com/huongdanjava/springenabletransactionmanagement/SpringEnabletransactionmanagementApplication.java
@@ -1,15 +1,21 @@
 package com.huongdanjava.springenabletransactionmanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
-public class SpringEnabletransactionmanagementApplication {
+// @EnableTransactionManagement
+public class SpringEnabletransactionmanagementApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringEnabletransactionmanagementApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    // TODO Auto-generated method stub
+
   }
 
 }
