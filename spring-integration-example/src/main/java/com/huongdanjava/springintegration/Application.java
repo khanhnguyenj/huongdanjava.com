0acc182e61b1bbe48dd5a77fc2874db9d85aa07a
Khanh Nguyen <nhkhanh2406@gmail.com>
2022-05-31 07:16:00 +0700

    Update project

--- spring-integration-example/src/main/java/com/huongdanjava/springintegration/Application.java
+++ spring-integration-example/src/main/java/com/huongdanjava/springintegration/Application.java
@@ -1,9 +1,11 @@
 package com.huongdanjava.springintegration;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
 
   public static void main(String[] args) {
-
+    new ClassPathXmlApplicationContext("spring-integration.xml");
   }
 
 }
