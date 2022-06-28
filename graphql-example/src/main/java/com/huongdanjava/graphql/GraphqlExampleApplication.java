a2715a278fff2c00afdc46f48ad136ef08a8bcee
Khanh Nguyen <nhkhanh2406@gmail.com>
2022-06-28 07:34:00 +0700

    Update project

--- graphql-example/src/main/java/com/huongdanjava/graphql/GraphqlExampleApplication.java
+++ graphql-example/src/main/java/com/huongdanjava/graphql/GraphqlExampleApplication.java
@@ -2,12 +2,22 @@ package com.huongdanjava.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @formatter:off
+@ComponentScan(
+    basePackages = {
+        "org.openapitools",
+        "com.huongdanjava.graphql",
+        "org.openapitools.configuration"
+    }
+)
+// @formatter:on
 public class GraphqlExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqlExampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GraphqlExampleApplication.class, args);
+  }
 
 }
---
+++ graphql-example/src/main/java/com/huongdanjava/graphql/web/impl/StudentsApiDelegateImpl.java
@@ -0,0 +1,7 @@
+package com.huongdanjava.graphql.web.impl;
+
+import com.huongdanjava.graphql.web.StudentsApiDelegate;
+
+public class StudentsApiDelegateImpl implements StudentsApiDelegate {
+
+}
