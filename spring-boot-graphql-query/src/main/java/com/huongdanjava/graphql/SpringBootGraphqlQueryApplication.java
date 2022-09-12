0258a3e4dbca3961c159650d709b668472720a3b
Khanh Nguyen <nhkhanh2406@gmail.com>
2022-09-12 11:16:19 +0700

    Share project

---
+++ spring-boot-graphql-query/src/main/java/com/huongdanjava/graphql/SpringBootGraphqlQueryApplication.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlQueryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootGraphqlQueryApplication.class, args);
+  }
+
+}
