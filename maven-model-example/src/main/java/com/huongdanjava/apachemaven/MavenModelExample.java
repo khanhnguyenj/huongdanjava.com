fdf81297c17ada94a1892130422b334a27426dc0
Khanh Nguyen <nhkhanh2406@gmail.com>
2023-03-30 12:29:55 +0700

    Update

--- maven-model-example/src/main/java/com/huongdanjava/apachemaven/MavenModelExample.java
+++ maven-model-example/src/main/java/com/huongdanjava/apachemaven/MavenModelExample.java
@@ -1,13 +1,25 @@
 package com.huongdanjava.apachemaven;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 public class MavenModelExample {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        String pomXml = "/Users/khanh/Documents/code/huongdanjava.com/spring-boot-actuator";
-        Reader reader = new FileReader(pomXml);
-    }
+  public static void main(String[] args) throws IOException, XmlPullParserException {
+    String pomXml = "/Users/khanh/Documents/code/huongdanjava.com/spring-boot-actuator/pom.xml";
+    Reader reader = new FileReader(pomXml);
+
+    MavenXpp3Reader xpp3Reader = new MavenXpp3Reader();
+    Model model = xpp3Reader.read(reader);
+
+    List<Plugin> plugins = model.getBuild().getPlugins();
+    plugins.forEach(p -> System.out.println(p.getArtifactId()));
+  }
+
 }
