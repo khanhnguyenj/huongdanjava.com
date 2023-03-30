0ce1e123881e639bca670d59da568c08e84a02a8
Khanh Nguyen <nhkhanh2406@gmail.com>
2023-03-30 08:24:25 +0700

    Update

--- spring-boot-actuator/src/main/java/com/huongdanjava/springbootactuator/SpringBootActuatorApplication.java
+++ spring-boot-actuator/src/main/java/com/huongdanjava/springbootactuator/SpringBootActuatorApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootActuatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootActuatorApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootActuatorApplication.class, args);
+  }
 
 }
 
