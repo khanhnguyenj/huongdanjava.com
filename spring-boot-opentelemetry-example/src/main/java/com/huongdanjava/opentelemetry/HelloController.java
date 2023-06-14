88872730d813a8f3e9755782b9903d09b7b5268f
Khanh Nguyen <nhkhanh2406@gmail.com>
2023-06-14 16:22:56 -0400

    Update

--- spring-boot-opentelemetry-example/src/main/java/com/huongdanjava/opentelemetry/HelloController.java
+++ spring-boot-opentelemetry-example/src/main/java/com/huongdanjava/opentelemetry/HelloController.java
@@ -2,13 +2,23 @@ package com.huongdanjava.opentelemetry;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
 
 @RestController
 public class HelloController {
- 
+
+  private Meter meter = GlobalOpenTelemetry.meterBuilder("io.opentelemetry.metrics.hello")
+      .build();
+
+  private LongCounter counter = meter.counterBuilder("helloCounter")
+      .build();
+
   @GetMapping("/hello")
   public String hello() {
+    counter.add(1);
     return "Hello";
   }
- 
+
 }
--- spring-boot-opentelemetry-example/src/main/java/com/huongdanjava/opentelemetry/SpringBootOpentelemetryExampleApplication.java
+++ spring-boot-opentelemetry-example/src/main/java/com/huongdanjava/opentelemetry/SpringBootOpentelemetryExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootOpentelemetryExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOpentelemetryExampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootOpentelemetryExampleApplication.class, args);
+  }
 
 }
