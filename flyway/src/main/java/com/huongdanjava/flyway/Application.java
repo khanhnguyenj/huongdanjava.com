1b08a0a833773a3fa0f2bff892050a9387040ee0
Khanh Nguyen <nhkhanh2406@gmail.com>
2023-07-08 08:59:32 -0400

    Update

--- flyway/src/main/java/com/huongdanjava/flyway/Application.java
+++ flyway/src/main/java/com/huongdanjava/flyway/Application.java
@@ -4,12 +4,12 @@ import org.flywaydb.core.Flyway;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Flyway flyway = Flyway.configure()
-            .dataSource("jdbc:postgresql://localhost:5432/student", "postgres", "123456")
-            .load();
+  public static void main(String[] args) {
+    Flyway flyway = Flyway.configure()
+        .dataSource("jdbc:postgresql://localhost:5432/student", "postgres", "123456")
+        .load();
 
-        flyway.migrate();
-    }
+    flyway.migrate();
+  }
 
 }
