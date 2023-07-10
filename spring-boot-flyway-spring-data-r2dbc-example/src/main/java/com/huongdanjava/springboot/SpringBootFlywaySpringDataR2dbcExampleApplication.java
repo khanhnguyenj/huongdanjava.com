df0b73988bf9bc920d7f6de42c9d925f829a2ff6
Khanh Nguyen <nhkhanh2406@gmail.com>
2023-07-09 22:41:37 -0400

    Update

---
+++ spring-boot-flyway-spring-data-r2dbc-example/src/main/java/com/huongdanjava/springboot/SpringBootFlywaySpringDataR2dbcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywaySpringDataR2dbcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlywaySpringDataR2dbcExampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-flyway-spring-data-r2dbc-example/src/test/java/com/huongdanjava/springboot/SpringBootFlywaySpringDataR2dbcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlywaySpringDataR2dbcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
