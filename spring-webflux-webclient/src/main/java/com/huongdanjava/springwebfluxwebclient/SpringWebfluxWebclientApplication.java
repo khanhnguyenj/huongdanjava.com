f65bb517282fe584e98722190c5cd223414837d3
Khanh Nguyen <nhkhanh2406@gmail.com>
2023-09-07 15:19:11 -0400

    Update

--- spring-webflux-webclient/src/main/java/com/huongdanjava/springwebfluxwebclient/SpringWebfluxWebclientApplication.java
+++ spring-webflux-webclient/src/main/java/com/huongdanjava/springwebfluxwebclient/SpringWebfluxWebclientApplication.java
@@ -4,27 +4,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringWebfluxWebclientApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWebfluxWebclientApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWebfluxWebclientApplication.class, args);
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
-		String url = "http://localhost:8080";
+  @Override
+  public void run(String... args) throws Exception {
+    String url = "http://localhost:8080";
 
-		WebClient webClient = WebClient.builder().baseUrl(url).build();
+    WebClient webClient = WebClient.builder().baseUrl(url).build();
 
-		Flux<String> flux = webClient.get()
-		    .uri("/students")
-		    .retrieve()
-		    .bodyToFlux(String.class);
+    // @formatter:off
+    Flux<String> flux = webClient.get()
+        .uri("/students")
+        .retrieve()
+        .bodyToFlux(String.class);
 
-		flux.subscribe(s -> System.out.println(s));
-	}
+    // @formatter:on
+    flux.subscribe(s -> System.out.println(s));
+  }
 }
