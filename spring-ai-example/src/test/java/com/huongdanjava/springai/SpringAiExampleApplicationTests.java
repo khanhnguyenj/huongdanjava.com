0bc3b396060c41acd254d23e84e3e469c2230801
Khanh Nguyen <nhkhanh2406@gmail.com>
2024-05-19 21:10:51 +0700

    Share project

---
+++ spring-ai-example/src/test/java/com/huongdanjava/springai/SpringAiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
