75cba467cc49211a7ed3ac3242147480b645a95d
Khanh Nguyen <nhkhanh2406@gmai.com>
2024-08-21 07:19:37 +0700

    Share project

---
+++ grpc-example/src/main/java/com/huongdanjava/grpc/GrpcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrpcExampleApplication.class, args);
+  }
+
+}
---
+++ grpc-example/src/test/java/com/huongdanjava/grpc/GrpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
