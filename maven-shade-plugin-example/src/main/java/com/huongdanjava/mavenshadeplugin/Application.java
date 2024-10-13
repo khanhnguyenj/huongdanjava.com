ce7fbfc54c7e42ad46471eb12ec0d072d7b505ae
Khanh Nguyen <nhkhanh2406@gmai.com>
2024-10-14 07:33:38 +0800

    Update

--- maven-shade-plugin-example/src/main/java/com/huongdanjava/mavenshadeplugin/Application.java
+++ maven-shade-plugin-example/src/main/java/com/huongdanjava/mavenshadeplugin/Application.java
@@ -10,19 +10,19 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
-        String version = getVersion();
+  public static void main(String[] args) throws IOException {
+    String version = getVersion();
 
-        System.out.println(StringUtils
-                .capitalize("hello from Huong Dan Java. Application version is " + version));
-    }
+    System.out.println(StringUtils
+        .capitalize("hello from Huong Dan Java. Application version is " + version));
+  }
 
-    private static String getVersion() throws IOException {
-        URLClassLoader cl = (URLClassLoader) Application.class.getClassLoader();
-        URL url = cl.findResource("META-INF/MANIFEST.MF");
-        Manifest manifest = new Manifest(url.openStream());
-        Attributes attr = manifest.getMainAttributes();
+  private static String getVersion() throws IOException {
+    URLClassLoader cl = (URLClassLoader) Application.class.getClassLoader();
+    URL url = cl.findResource("META-INF/MANIFEST.MF");
+    Manifest manifest = new Manifest(url.openStream());
+    Attributes attr = manifest.getMainAttributes();
 
-        return attr.getValue("Implementation-Version");
-    }
+    return attr.getValue("Implementation-Version");
+  }
 }
