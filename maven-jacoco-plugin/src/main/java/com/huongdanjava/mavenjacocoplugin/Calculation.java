af2a09de41bd244575786e169f185e0f7b0a89cf
Khanh Nguyen <nhkhanh2406@gmai.com>
2024-10-14 21:35:14 +0800

    Update

--- maven-jacoco-plugin/src/main/java/com/huongdanjava/mavenjacocoplugin/Calculation.java
+++ maven-jacoco-plugin/src/main/java/com/huongdanjava/mavenjacocoplugin/Calculation.java
@@ -2,16 +2,16 @@ package com.huongdanjava.mavenjacocoplugin;
 
 public class Calculation {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+  public int add(int a, int b) {
+    return a + b;
+  }
 
-	public int sub(int a, int b) {
-		if (a >= b) {
-			return a - b;
-		}
+  public int sub(int a, int b) {
+    if (a >= b) {
+      return a - b;
+    }
 
-		return b - a;
-	}
+    return b - a;
+  }
 
 }
--- maven-jacoco-plugin/src/test/java/com/huongdanjava/mavenjacocoplugin/CalculationTest.java
+++ maven-jacoco-plugin/src/test/java/com/huongdanjava/mavenjacocoplugin/CalculationTest.java
@@ -1,27 +1,29 @@
 package com.huongdanjava.mavenjacocoplugin;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class CalculationTest {
 
-	private Calculation calculation;
+  private static Calculation calculation;
 
-	@Before
-	public void init() {
-		calculation = new Calculation();
-	}
+  @BeforeAll
+  public static void init() {
+    calculation = new Calculation();
+  }
 
-	@Test
-	public void testAdd() {
-		assertEquals(4, calculation.add(1, 3));
-	}
+  @Test
+  public void testAdd() {
+    assertEquals(4, calculation.add(1, 3));
+  }
 
-	@Test
-	public void testSub() {
-		assertEquals(2, calculation.sub(7, 5));
-	}
+  @Test
+  public void testSub() {
+    assertEquals(2, calculation.sub(7, 5));
+
+    assertEquals(9, calculation.sub(3, 12));
+  }
 
 }
