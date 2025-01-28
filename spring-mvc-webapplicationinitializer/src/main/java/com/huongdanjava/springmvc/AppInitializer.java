d627fad9d8df2e101d4402214f1047dc92afddd4
Khanh Nguyen <nhkhanh2406@gmai.com>
2025-01-28 07:41:37 +0700

    Update project

--- spring-mvc-webapplicationinitializer/src/main/java/com/huongdanjava/springmvc/AppInitializer.java
+++ spring-mvc-webapplicationinitializer/src/main/java/com/huongdanjava/springmvc/AppInitializer.java
@@ -1,14 +1,14 @@
 package com.huongdanjava.springmvc;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
 
 public class AppInitializer implements WebApplicationInitializer {
 
