57f1aab9c4ba0e800ebd94359da54137108b404a
Khanh Nguyen <nhkhanh2406@gmai.com>
2025-07-08 17:35:02 +0800

    Share project

---
+++ spring-cloud-consul-service-discovery/src/main/java/com/huongdanjava/springcloud/consul/SpringCloudConsulServiceDiscoveryApplication.java
@@ -0,0 +1,32 @@
+package com.huongdanjava.springcloud.consul;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudConsulServiceDiscoveryApplication {
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudConsulServiceDiscoveryApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner commandLineRunner() {
+    return args -> {
+      List<ServiceInstance> instances = discoveryClient.getInstances("huongdanjava-service");
+      System.out.println(instances.getFirst().getUri());
+    };
+  }
+
+}
---
+++ spring-cloud-consul-service-discovery/src/test/java/com/huongdanjava/springcloud/consul/SpringCloudConsulServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springcloud.consul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConsulServiceDiscoveryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
