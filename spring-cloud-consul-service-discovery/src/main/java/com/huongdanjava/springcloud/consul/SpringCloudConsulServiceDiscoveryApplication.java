0a524ae41d48356e69c14b6f2276baf808bc0ab6
Khanh Nguyen <nhkhanh2406@gmai.com>
2025-10-12 17:02:09 +0800

    Update project

--- spring-cloud-consul-service-discovery/src/main/java/com/huongdanjava/springcloud/consul/SpringCloudConsulServiceDiscoveryApplication.java
+++ spring-cloud-consul-service-discovery/src/main/java/com/huongdanjava/springcloud/consul/SpringCloudConsulServiceDiscoveryApplication.java
@@ -7,17 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SpringCloudConsulServiceDiscoveryApplication {
 
   @Autowired
   private DiscoveryClient discoveryClient;
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     SpringApplication.run(SpringCloudConsulServiceDiscoveryApplication.class, args);
   }
 
