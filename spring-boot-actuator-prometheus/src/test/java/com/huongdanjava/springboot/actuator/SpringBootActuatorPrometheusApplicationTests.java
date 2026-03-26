c4520c78bcb87d7f03594929f5aac995c483dfc5
Khanh Nguyen <nhkhanh2406@gmai.com>
2026-03-27 06:40:07 +0700

    Share project

---
+++ spring-boot-actuator-prometheus/src/test/java/com/huongdanjava/springboot/actuator/SpringBootActuatorPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdanjava.springboot.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorPrometheusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
