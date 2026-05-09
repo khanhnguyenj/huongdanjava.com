85c449b282fa7b1765b4819f96499b271f04229c
Khanh Nguyen <nhkhanh2406@gmai.com>
2026-05-09 07:51:55 +0700

    Update project

--- spring-cloud-stream-rabbitmq-example/src/main/java/com/huongdanjava/springcloudstream/SpringCloudStreamRabbitmqExampleApplication.java
+++ spring-cloud-stream-rabbitmq-example/src/main/java/com/huongdanjava/springcloudstream/SpringCloudStreamRabbitmqExampleApplication.java
@@ -2,21 +2,34 @@ package com.huongdanjava.springcloudstream;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudStreamRabbitmqExampleApplication {
+
+  @Autowired
+  private StreamBridge streamBridge;
 
   static void main(String[] args) {
     SpringApplication.run(SpringCloudStreamRabbitmqExampleApplication.class, args);
   }
 
   @Bean
-  public Supplier<String> sendSupplier() {
-    return () -> "Hello World";
+  CommandLineRunner runner() {
+    return args -> {
+      streamBridge.send("sendSupplier-out-0", "Hello World");
+    };
   }
+
+//  @Bean
+//  public Supplier<String> sendSupplier() {
+//    return () -> "Hello World";
+//  }
 
   @Bean
   public Consumer<String> receiveConsumer() {
