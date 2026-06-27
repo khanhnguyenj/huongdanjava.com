a27fefd17259724475176a4260ffbf158292d0c3
Khanh Nguyen <nhkhanh2406@gmai.com>
2026-06-27 12:15:19 +0700

    Update project

--- spring-ai-example/src/main/java/com/huongdanjava/springai/AppConfiguration.java
+++ spring-ai-example/src/main/java/com/huongdanjava/springai/AppConfiguration.java
@@ -1,65 +1,24 @@
 package com.huongdanjava.springai;
 
-import io.micrometer.observation.ObservationRegistry;
-import java.util.List;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.model.tool.ToolCallingChatOptions;
-import org.springframework.ai.model.tool.ToolCallingManager;
-import org.springframework.ai.model.tool.ToolExecutionResult;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.ai.tool.definition.ToolDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class AppConfiguration {
 
   @Bean
-  OpenAiApi openAiApi() {
-    return new OpenAiApi.Builder()
-        .apiKey("<YOUR_API_KEY_HERE>")
+  OpenAiChatOptions chatOptions() {
+    return OpenAiChatOptions.builder()
+        .apiKey("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")
+        .model(com.openai.models.ChatModel.GPT_5_NANO.asString())
         .build();
   }
 
   @Bean
-  ChatModel chatModel(OpenAiApi openAiApi, ToolCallingManager toolCallingManager,
-      RetryTemplate retryTemplate, ObservationRegistry observationRegistry) {
-    OpenAiChatOptions openAiChatOptions = OpenAiChatOptions.builder()
-        .model(OpenAiApi.ChatModel.GPT_5_NANO)
-        .build();
-
-    return new OpenAiChatModel(openAiApi, openAiChatOptions, toolCallingManager, retryTemplate,
-        observationRegistry);
-  }
-
-  @Bean
-  RetryTemplate retryTemplate() {
-    return new RetryTemplate();
-  }
-
-  @Bean
-  ToolCallingManager toolCallingManager() {
-    return new ToolCallingManager() {
-      @Override
-      public List<ToolDefinition> resolveToolDefinitions(ToolCallingChatOptions chatOptions) {
-        return List.of();
-      }
-
-      @Override
-      public ToolExecutionResult executeToolCalls(Prompt prompt, ChatResponse chatResponse) {
-        return null;
-      }
-    };
-  }
-
-  @Bean
-  ObservationRegistry observationRegistry() {
-    return ObservationRegistry.create();
+  ChatModel chatModel(OpenAiChatOptions chatOptions) {
+    return OpenAiChatModel.builder().options(chatOptions).build();
   }
 }
-
--- spring-ai-example/src/main/java/com/huongdanjava/springai/Application.java
+++ spring-ai-example/src/main/java/com/huongdanjava/springai/Application.java
@@ -8,13 +8,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
   static void main(String[] args) {
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-        AppConfiguration.class);
+    AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(AppConfiguration.class);
 
     ChatModel chatModel = context.getBean(ChatModel.class);
-    ChatResponse chatResponse = chatModel.call(
-        new Prompt("Do you know about Huong Dan Java website?"));
+    ChatResponse chatResponse =
+        chatModel.call(new Prompt("Do you know about Huong Dan Java website?"));
     System.out.println(chatResponse.getResult());
   }
-
 }
