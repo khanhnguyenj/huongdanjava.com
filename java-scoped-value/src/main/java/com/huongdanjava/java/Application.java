578acb4f919f13cb67d21211f97eae01144e6a80
Khanh Nguyen <nhkhanh2406@gmai.com>
2026-07-07 12:26:46 +0700

    Update project

--- java-scoped-value/src/main/java/com/huongdanjava/java/Application.java
+++ java-scoped-value/src/main/java/com/huongdanjava/java/Application.java
@@ -4,17 +4,19 @@ public class Application {
 
   public static final ScopedValue<String> LOGGED_USER = ScopedValue.newInstance();
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     String user = "Khanh";
 
     StudentManagement sm = new StudentManagement();
+
     UserService us = new UserService();
 
-    ScopedValue.where(LOGGED_USER, user).run(()-> {
-          sm.getLoggedUserInfo();
-          us.doSomeThing();
-        }
-    );
+    ScopedValue.where(LOGGED_USER, user)
+        .run(
+            () -> {
+              sm.getLoggedUserInfo();
+              us.doSomeThing();
+            });
     us.doSomeThing();
   }
 }
