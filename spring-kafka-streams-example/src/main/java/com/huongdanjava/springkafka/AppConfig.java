a709344f060d60258483773da0801a8174fe3abc
Khanh Nguyen <nhkhanh2406@gmai.com>
2026-07-14 10:25:46 +0700

    Update project

--- spring-kafka-streams-example/src/main/java/com/huongdanjava/springkafka/AppConfig.java
+++ spring-kafka-streams-example/src/main/java/com/huongdanjava/springkafka/AppConfig.java
@@ -4,16 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.errors.DefaultProductionExceptionHandler;
-import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
-import org.apache.kafka.streams.errors.LogAndContinueProcessingExceptionHandler;
-import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
-import org.springframework.kafka.config.StreamsBuilderFactoryBeanConfigurer;
 
 @Configuration
 @EnableKafkaStreams
@@ -26,38 +21,7 @@ public class AppConfig {
     props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    props.put(
-        StreamsConfig.PROCESSING_EXCEPTION_HANDLER_CLASS_CONFIG,
-        LogAndContinueProcessingExceptionHandler.class.getName());
-    props.put(
-        StreamsConfig.PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG,
-        DefaultProductionExceptionHandler.class.getName());
-    props.put(
-        StreamsConfig.DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
-        LogAndContinueExceptionHandler.class.getName());
 
     return new KafkaStreamsConfiguration(props);
   }
-
-  @Bean
-  public StreamsBuilderFactoryBeanConfigurer streamsCustomizer() {
-    return factoryBean ->
-        factoryBean.setStreamsUncaughtExceptionHandler(
-            exception -> {
-              exception.printStackTrace();
-
-              return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD;
-            });
-  }
-
-  //  @Bean
-  //  KafkaStreamsCustomizer kafkaStreamsCustomizer() {
-  //    return kafkaStreams ->
-  //        kafkaStreams.setUncaughtExceptionHandler(
-  //            exception -> {
-  //              exception.printStackTrace();
-  //
-  //              return StreamThreadExceptionResponse.REPLACE_THREAD;
-  //            });
-  //  }
 }
